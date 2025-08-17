2ca090801bb8b76be864ba0d8a6a01e35077f6db
Azeezshk <shaikabdulazeez877@gmail.com>
2025-08-17 23:48:12 +0530

    variables added again

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This is the conversation added
+
+echo "Suresh :: Hi Ramesh !"
+echo "Ramesh :: Hi Suresh, How are you ?"
+echo "Suresh :: I am good, what about you?"
+echo "Ramesh :: All good man !"
