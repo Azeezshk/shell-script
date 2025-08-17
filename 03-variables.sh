708aaccbe8db67c3c655d1cc088d1471ba11da69
Azeezshk <shaikabdulazeez877@gmail.com>
2025-08-17 23:59:17 +0530

    variables added with $

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This is the conversation added
+PERSON1=Suresh
+PERSON2=Ramesh
+
+echo "$PERSON1 :: Hi $PERSON2 !"
+echo "$PERSON2 :: Hi $PERSON1, How are you ?"
+echo "$PERSON1 :: I am good, what about you?"
+echo "$PERSON2 :: All good man !"
