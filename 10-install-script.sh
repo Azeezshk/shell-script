1d4f387dd2fd7d02ccb8ffb25841e9bbb93eccdf
Azeezshk <shaikabdulazeez877@gmail.com>
2025-08-18 22:04:51 +0530

    variables sum

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -2,11 +2,10 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ "$USERID" -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
-    
-    exit 1 #other than 0 (for exit status whether last process is success or not)
+    exit 1
 fi
 
 dnf install mysql -y
@@ -17,6 +16,7 @@ then
 else
     echo "Installing MySQL........Success"
 fi
+
 dnf install git -y
 if [ $? -ne 0 ]
 then 
