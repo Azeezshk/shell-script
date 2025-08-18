be16a6cf3a445e2b7d9d4ec0ac251e5129faba02
Azeezshk <shaikabdulazeez877@gmail.com>
2025-08-18 15:44:09 +0530

    variables sum

--- 06-variables-sum.sh
+++ 06-variables-sum.sh
@@ -2,7 +2,8 @@
 NUMBER1=$1
 NUMBER2=$2
 
-
+TIMESTAMP=$(date)
+echo "Script is executed at : $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "THE SUM OF numbers is :" $SUM
+echo "THE SUM OF numbers is : $SUM"
