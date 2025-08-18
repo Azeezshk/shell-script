74e572fc17c99eb7a2b792e2a3ba62fae8831c0f
Azeezshk <shaikabdulazeez877@gmail.com>
2025-08-18 15:54:47 +0530

    variables sum

---
+++ 07-Arrays.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+MOVIES=("Coolie" "WAR 2" "Marco")
+
+#here coolie is 0, war2 is 1, marco is 3. as array list numbers start from 0 to 9
+
+echo "The first movie is: ${MOVIES[0]}"
+echo "The Second movie is: ${MOVIES[1]}"
+echo "The Third movie is: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
