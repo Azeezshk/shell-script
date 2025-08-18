3e8c53d49048369ff0309d16057260ac7838dc78
Azeezshk <shaikabdulazeez877@gmail.com>
2025-08-18 19:47:46 +0530

    variables sum

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,13 +1,12 @@
-#!bin/bash
+#!/bin/bash
 
-NUMBER1=$1
+NUMBER=$1
 
-#gt greater tha, lt- less than, ge -great than or equal to, le - less than or equal to, 
-#eq - equal to
+# -gt, -lt, -eq, -ge, -le
 
 if [ $NUMBER -gt 100 ]
 then
     echo "Given number is greater than 100"
-else 
+else
     echo "Given number is less than or equal to 100"
 fi
