1f91e0128ffb77c3bc596186b52f2e8e044a45d9
Azeezshk <shaikabdulazeez877@gmail.com>
2025-09-06 21:21:08 +0530

    practice more scripts

--- 05-variables-inputvar.sh
+++ 05-variables-inputvar.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-echo "Please enter your Username :: "
+# echo "Please enter your Username :: "
 
-read -s USERNAME
+# read -s USERNAME
 
-echo "username entered: $USERNAME"
+# echo "username entered: $USERNAME"
 
-echo "please enter your password :: "
+# echo "please enter your password :: "
 
-read -s PASSWORD
+# read -s PASSWORD
+
+
+
+# read -p "Please enter your Username :: " USERNAME
+# echo " Entered Username is $USERNAME "
+
+# read -s -p "Please enter your password :: " PASSOWRD
+# echo " Login successfull ! "
+
+CORRECT_USERNAME="Admin"
+CORRECT_PASSWORD="azeez15176"
+
+read -p " Please Enter Username : " USERNAME
+echo " Entered Username is $USERNAME "
+
+read -s -p " Please enter your Password : " PASSWORD
+echo
+
+if [[ "$USERNAME" == "$CORRECT_USERNAME" && "$PASSWORD" == "$CORRECT_PASSWORD" ]];
+then
+echo " Login successful "
+else
+echo " Login not successfull....Please enter valid details "
+fi
