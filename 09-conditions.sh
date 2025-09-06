7b2aced24cd75a79f0250ced591d54544283c284
Azeezshk <shaikabdulazeez877@gmail.com>
2025-09-06 22:05:14 +0530

    practice more scripts

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,16 +5,16 @@ NUMBER=$1
 #gt greater tha, lt- less than, ge -great than or equal to, le - less than or equal to, 
 #eq - equal to
 
-# if [ $NUMBER -gt 100 ]
-# then
-#     echo "Given number is greater than 100"
-# else 
-#     echo "Given number is less than or equal to 100"
-# fi
-
-if [ $NUMBER -lt 100 ];
+if [ $NUMBER -gt 100 ]
 then
-echo "The number is less than 100"
-else
-echo "The number given is greater than or equal to 100"
+    echo "Given number is greater than 100"
+else 
+    echo "Given number is less than or equal to 100"
 fi
+
+# if [ $NUMBER -lt 100 ];
+# then
+# echo "The number is less than 100"
+# else
+# echo "The number given is greater than or equal to 100"
+# fi
