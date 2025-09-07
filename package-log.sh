b35041e52f2a37539c268bcdd9133f009f48ba12
Azeezshk <shaikabdulazeez877@gmail.com>
2025-09-08 00:22:47 +0530

    Log file modified

--- package-log.sh
+++ package-log.sh
@@ -7,7 +7,7 @@ B="\e[34m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"   # Directory where all script logs will be stored
-LOG_FILE=$(basename "$0" | cut -d "." -f1)   # Get the script name (without extension) to use as log file prefix
+LOG_FILE=$(echo "$0" | cut -d "." -f1)   # Get the script name (without extension) to use as log file prefix
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)   # Current date and time, used to make log file unique
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"   # Full path of the log file (folder + script name + timestamp)
 
@@ -25,7 +25,7 @@ VALIDATE(){
 }
 
 
-echo "Script executed at : $TIMESTAMP" 
+echo "Script executed at : $TIMESTAMP" &>> "$LOG_FILE_NAME"
 
 # Root check
 USERID=$(id -u)
@@ -44,25 +44,23 @@ chmod 755 "$LOGS_FOLDER"
 
 
 # Example install: MySQL 
-
-dnf list installed mysql
+dnf list installed mysql                                    &>> "$LOG_FILE_NAME"
 
 if [ $? -ne 0 ];
 then
-    dnf install mysql -y
-    VALIDATE $? " $G Installing MySQL $N"
+    dnf install mysql -y                                    &>> "$LOG_FILE_NAME"
+    VALIDATE $? " $G Installing MySQL $N" 
 else
     echo -e "$Y MySQL is already Installed...! $N"
 fi
 
 
 # Example install: Git 
-
-dnf list installed git
+dnf list installed git                                      &>> "$LOG_FILE_NAME"
 
 if [ $? -ne 0 ];
 then
-    dnf install git -y
+    dnf install git -y                                      &>> "$LOG_FILE_NAME"
     VALIDATE $? "$Y Installing Git $N"
 else
     echo -e "$Y Git is Already Installed....! $N"
