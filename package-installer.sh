c522efc849245cd27fd141c3a467411a9455a823
Azeezshk <shaikabdulazeez877@gmail.com>
2025-09-07 18:22:31 +0530

    Package installing script modified

---
+++ package-installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ];
+then
+echo "You must have sudo access to run the script"
+exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ];
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ];
+    then
+    echo "Installing MySQL....Failure"
+    else
+    echo "Insatalling MYSQL.......Success"
+    fi
+else 
+echo "MySQL is already Installed....."
+fi
