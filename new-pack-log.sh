2469085125068e58458b988f8a9ba50db76f71d3
Azeezshk <shaikabdulazeez877@gmail.com>
2025-09-08 20:07:25 +0530

    new log file script addded

--- new-pack-log.sh
+++ new-pack-log.sh
@@ -6,14 +6,14 @@ Y=\e[33m
 B="\e[34m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscriipt-logs"
-LOG_FILE=$(echo "$0" | cut -d "." f1)
-TIMESTAMP=$(date +Y%-m%-d%-H%-M%-S%)
-LOG_FILE_NAME=($LOGS_FOLDER/$LOG_FILE+$TIMESTAMP.log)
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo "$0" | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME=($LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log)
 
 
 VALIDATE(){
-    if [ $1 -ne 0 ];
+    if [ $1 -ne 0 ]
     then
         echo "$2.........Failure"
         exit 1
@@ -27,7 +27,7 @@ echo "Script executed at : $TIMESTAMP"
 #Root Check
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ];
+if [ $USERID -ne 0 ]
 then
     echo "You must have root access to run the script"
     exit 1
@@ -45,7 +45,7 @@ chmod 755 "$LOGS_FOLDER"
 
 dnf list installed mysql
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]
 then
     dnf install mysql -y
     VALIDATE $? "Installing MySQL"
@@ -57,7 +57,7 @@ fi
 
 dnf list installed nginx
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]
 then
     dnf install nginx -y
     VALIDATE $? "Installing Nginx"
