34118ef5fa346070c35890eb88d9b58ae6f9f1eb
Azeezshk <shaikabdulazeez877@gmail.com>
2025-09-10 19:47:32 +0530

    disk usage script added

--- disk-usage.sh
+++ disk-usage.sh
@@ -16,4 +16,4 @@ done <<< $DISK_USAGE
 
 echo -e "Message :$MSG"
 
-echo $MSG | mutt -s "High Disk Usage " shaikabdulazeez176@gmail.com
+echo "$MSG" | mutt -s "High Disk Usage " shaikabdulazeez176@gmail.com
